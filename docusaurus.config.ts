9b2a075c9cb41e733b044ec114738b09563dfb79
pomili <pomili@pomilideMacBook-Air.local>
2026-05-22 00:12:47 +0800

    更换 favicon 和 logo 为菜鸡 SVG

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'pomili',
   tagline: '探索 · 构建 · 分享',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
