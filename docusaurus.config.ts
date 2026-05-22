8f80c4cea55e48846c40af016dfa2c2c24f2aadf
pomili <pomili@pomilideMacBook-Air.local>
2026-05-22 13:02:10 +0800

    添加英文多语言支持

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -28,7 +28,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'zh-Hans',
-    locales: ['zh-Hans'],
+    locales: ['zh-Hans','en'],
   },
 
   presets: [
